ea4adde26862592ae3b838b10bd7a28d046a658b
Bogdan <zykovbogdan07@gmail.com>
2022-01-10 14:08:44 +0300

    Initial Commit

---
+++ SecureApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SecureApp
+//
+//  Created by Богдан Зыков on 10.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SecureApp/SecureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureAppApp.swift
+//  SecureApp
+//
+//  Created by Богдан Зыков on 10.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SecureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
